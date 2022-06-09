dc1e6d8e5dccb588c6742ff2559f915b5b4f3020
Ulyssbat <stewartbatallasminda@gmail.com>
2022-06-10 01:18:20 +0200

    creamos express y lo intentamos vincular a react. Puede que el error este en react y el host

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -9,13 +8,12 @@ class App extends Component {
   }
 
   callAPI() {
-    fetch("http://localhost:9000/testAPI")
+    fetch("http://localhost:9000/index")
       .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }))
-      .catch(err => err);
+      .then(res => this.setState({ apiResponse: res }));
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.callAPI();
   }
 
@@ -26,7 +24,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">;{this.state.apiResponse}</p>
+        <p className="App-intro">"hello"</p>
       </div>
     );
   }
