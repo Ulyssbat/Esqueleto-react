d86e3e4b3333708330049c46d5088290be4133e3
Ulyssbat <stewartbatallasminda@gmail.com>
2022-06-10 20:39:14 +0200

    conseguimos conectar react con nodejs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   constructor(props) {
@@ -9,11 +10,11 @@ class App extends Component {
 
   callAPI() {
     fetch("http://localhost:9000/index")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }));
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }))
+      .catch((err) => err)
   }
-
-  componentWillMount() {
+  componentDidMount() {
     this.callAPI();
   }
 
@@ -24,7 +25,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">"hello"</p>
+        <p className="App-intro">{this.state.apiResponse}</p>
       </div>
     );
   }
